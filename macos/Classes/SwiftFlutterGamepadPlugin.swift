a28a5e130a8a292f1075d5c891ac840d60d777a5
dkbast <hey@damianbast.com>
2020-12-19 00:48:57 +0100

    uploading copies

---
+++ macos/Classes/SwiftFlutterGamepadPlugin.swift
@@ -0,0 +1,183 @@
+#if os(iOS) || os(tvOS)
+import Flutter
+import UIKit
+#else // os(macOS)
+import FlutterMacOS
+#endif
+import GameController
+
+enum Button : Int {
+  case a = 0
+  case b
+  case x
+  case y
+  case dpadUp
+  case dpadDown
+  case dpadLeft
+  case dpadRight
+  case menu
+  case options
+  case leftThumbstickButton
+  case rightThumbstickButton
+  case leftShoulder
+  case rightShoulder
+  case leftTrigger
+  case rightTrigger
+}
+
+enum Thumbstick : Int {
+  case left = 0
+  case right
+}
+
+@objc class GamepadStreamHandler: NSObject, FlutterStreamHandler {
+  private var eventSink: FlutterEventSink?
+  private var gamepads: [GCExtendedGamepad] = []
+
+  // In practice, `isAttachedToDevice` sometimes throws for some godforsaken internal reason (array index out of bounds),
+  // but it isn't marked `throws`, so `try?` will cause a warning. This wrapper *is* marked `throws`, to remedy this.
+  private func gamepadIsAttachedToDevice(gamepad: GCExtendedGamepad) throws -> Bool? {
+    return gamepad.controller?.isAttachedToDevice
+  }
+
+  private func gamepadInfoDictionary(gamepad: GCExtendedGamepad) -> [String: Any] {
+    var result: [String: Any] = [:];
+    result["isAttachedToDevice"] = try? gamepadIsAttachedToDevice(gamepad: gamepad) as Any;
+    // result["playerIndex"] = gamepad.controller?.playerIndex.rawValue as Any;
+    result["vendorName"] = gamepad.controller?.vendorName as Any;
+    if #available(iOS 13.0, tvOS 13.0, OSX 10.15, *) {
+      result["productCategory"] = gamepad.controller?.productCategory as Any;
+      // result["isSnapshot"] = gamepad.controller?.isSnapshot as Any;
+    }
+    result["id"] = gamepad.hash;
+    return result
+  }
+
+  public func allGamepadInfoDictionaries() -> [[String: Any]] {
+    return gamepads.map(gamepadInfoDictionary)
+  }
+
+  public func onListen(withArguments arguments: Any?, eventSink events: @escaping FlutterEventSink) -> FlutterError? {
+    eventSink = events
+
+    // Initial read:
+    for gc in GCController.controllers() {
+      if let eg = gc.extendedGamepad { self.gamepadConnected(gamepad: eg) }
+    }
+
+    // Then start listening:
+    NotificationCenter.default.addObserver(forName: .GCControllerDidConnect, object: nil, queue: .main) { (note) in
+      guard let gc = note.object as? GCController else {
+        print("GCControllerDidConnect: non-controller object?")
+        return
+      }
+      guard let eg = gc.extendedGamepad else {
+        print("GCControllerDidConnect: Ignoring non-extendedGamepad controller; vendorName is \(String(describing: gc.vendorName)).")
+        return
+      }
+      print("GCControllerDidConnect: Adding extendedGamepad with vendorName \(String(describing: gc.vendorName)).")
+      self.gamepadConnected(gamepad: eg)
+    }
+
+    NotificationCenter.default.addObserver(forName: .GCControllerDidDisconnect, object: nil, queue: .main) { (note) in
+      guard let gc = note.object as? GCController else {
+        print("GCControllerDidDisconnect: non-controller object?")
+        return
+      }
+      guard let eg = gc.extendedGamepad else {
+        print("GCControllerDidDisconnect: Ignoring non-extendedGamepad controller; vendorName is \(String(describing: gc.vendorName)).")
+        return
+      }
+      print("GCControllerDidDisconnect: Removing extendedGamepad with vendorName \(String(describing: gc.vendorName)).")
+      self.gamepadDisconnected(gamepad: eg)
+    }
+
+    GCController.startWirelessControllerDiscovery(completionHandler: {})
+    print("Started wireless controller discovery")
+    return nil
+  }
+
+  public func onCancel(withArguments arguments: Any?) -> FlutterError? {
+    GCController.stopWirelessControllerDiscovery()
+    print("Stopped wireless controller discovery")
+    NotificationCenter.default.removeObserver(self)
+    eventSink = nil
+    return nil
+  }
+
+  private func gamepadConnected(gamepad: GCExtendedGamepad) {
+    guard let eventSink = eventSink else { return }
+    func buttonHandler(_ button: Button) -> GCControllerButtonValueChangedHandler {
+      return { (_, value, pressed) in eventSink(["event": "button", "gamepadId": gamepad.hash, "button": button.rawValue, "value": value, "pressed": pressed]) }
+    }
+    func thumbstickHandler(_ thumbstick: Thumbstick) -> GCControllerDirectionPadValueChangedHandler {
+      // We flip the Y-axis direction here: our library has Y+ = down in line with Flutter's coordinate system, but iOS has Y+ = up.
+      return { (_, x, y) in eventSink(["event": "thumbstick", "gamepadId": gamepad.hash, "thumbstick": thumbstick.rawValue, "x": x, "y": -y]) }
+    }
+    eventSink(["event": "gamepadConnected", "gamepadId": gamepad.hash, "gamepadInfo": gamepadInfoDictionary(gamepad: gamepad)])
+    gamepad.buttonA.valueChangedHandler = buttonHandler(.a)
+    gamepad.buttonB.valueChangedHandler = buttonHandler(.b)
+    gamepad.buttonX.valueChangedHandler = buttonHandler(.x)
+    gamepad.buttonY.valueChangedHandler = buttonHandler(.y)
+    if #available(iOS 13.0, tvOS 13.0, OSX 10.15, *) {
+      gamepad.buttonOptions?.valueChangedHandler = buttonHandler(.options)
+      gamepad.buttonMenu.valueChangedHandler = buttonHandler(.menu)
+    } else {
+      gamepad.controller?.controllerPausedHandler = { (_) in
+        eventSink(["event": "button", "gamepadId": gamepad.hash, "button": Button.menu.rawValue, "value": 1.0])
+        eventSink(["event": "button", "gamepadId": gamepad.hash, "button": Button.menu.rawValue, "value": 0.0])
+      }
+    }
+    gamepad.leftShoulder.valueChangedHandler = buttonHandler(.leftShoulder)
+    gamepad.leftTrigger.valueChangedHandler = buttonHandler(.leftTrigger)
+    gamepad.rightShoulder.valueChangedHandler = buttonHandler(.rightShoulder)
+    gamepad.rightTrigger.valueChangedHandler = buttonHandler(.rightTrigger)
+    gamepad.dpad.up.valueChangedHandler = buttonHandler(.dpadUp)
+    gamepad.dpad.down.valueChangedHandler = buttonHandler(.dpadDown)
+
+    gamepad.dpad.left.valueChangedHandler = buttonHandler(.dpadLeft)
+    gamepad.dpad.right.valueChangedHandler = buttonHandler(.dpadRight)
+    if #available(iOS 12.1, tvOS 12.1, OSX 10.14.1, *) {
+      gamepad.leftThumbstickButton?.valueChangedHandler = buttonHandler(.leftThumbstickButton)
+      gamepad.rightThumbstickButton?.valueChangedHandler = buttonHandler(.rightThumbstickButton)
+    }
+    gamepad.leftThumbstick.valueChangedHandler = thumbstickHandler(.left)
+    gamepad.rightThumbstick.valueChangedHandler = thumbstickHandler(.right)
+    gamepads.append(gamepad);
+  }
+
+  private func gamepadDisconnected(gamepad: GCExtendedGamepad) {
+    guard let eventSink = eventSink else { return }
+    eventSink(["event": "gamepadDisconnected", "gamepadId": gamepad.hash, "gamepadInfo": gamepadInfoDictionary(gamepad: gamepad)])
+    gamepads.removeAll { $0 == gamepad };
+  }
+}
+
+public class SwiftFlutterGamepadPlugin: NSObject, FlutterPlugin {
+  var gamepadStreamHandler: GamepadStreamHandler = GamepadStreamHandler()
+
+  public static func register(with registrar: FlutterPluginRegistrar) {
+#if os(iOS) || os(tvOS)
+    let channel = FlutterMethodChannel(name: "com.rainway.flutter_gamepad/methods", binaryMessenger: registrar.messenger())
+#else
+    let channel = FlutterMethodChannel(name: "com.rainway.flutter_gamepad/methods", binaryMessenger: registrar.messenger)
+#endif
+    let instance = SwiftFlutterGamepadPlugin()
+    registrar.addMethodCallDelegate(instance, channel: channel)
+
+#if os(iOS) || os(tvOS)
+    let eventChannel = FlutterEventChannel(name: "com.rainway.flutter_gamepad/events", binaryMessenger: registrar.messenger())
+#else
+    let eventChannel = FlutterEventChannel(name: "com.rainway.flutter_gamepad/events", binaryMessenger: registrar.messenger)
+#endif
+    eventChannel.setStreamHandler(instance.gamepadStreamHandler)
+  }
+
+  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+    if (call.method == "gamepads") {
+      result(gamepadStreamHandler.allGamepadInfoDictionaries());
+    } else {
+      result(FlutterMethodNotImplemented);
+    }
+  }
+}
